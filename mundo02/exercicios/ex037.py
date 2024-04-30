5224fc748dd6418b23cdf254f8c66fbbdbcf1e1e
ludosantana <lucas.d.santana@hotmail.com>
2024-04-30 07:55:19 -0300

    Aula python

--- mundo02/exercicios/ex037.py
+++ mundo02/exercicios/ex037.py
@@ -16,11 +16,11 @@ escolha = int(input("Qual opção você deseja? "))
 def opcoes():
     global escolha
     if escolha == 1:
-        print(f"O número \033[1;35m{entrada}\033[m em binário é: \033[1;32m{bin(entrada)}\033[m.")
+        print(f"O número \033[1;35m{entrada}\033[m em binário é: \033[1;32m{bin(entrada)[2:]}\033[m.")
     elif escolha == 2:
-        print(f"O número \033[1;35m{entrada}\033[m em octal é: \033[1;33m{oct(entrada)}\033[m.")
+        print(f"O número \033[1;35m{entrada}\033[m em octal é: \033[1;33m{oct(entrada)[2:]}\033[m.")
     elif escolha == 3:
-        print(f"O número \033[1;35m{entrada}\033[m em hexadecimal é: \033[1;34m{hex(entrada)}\033[m.")
+        print(f"O número \033[1;35m{entrada}\033[m em hexadecimal é: \033[1;34m{hex(entrada)[2:]}\033[m.")
     else:
         print("Digite uma das opções solicitadas.")
         escolha = int(input("Qual opção você deseja? "))
