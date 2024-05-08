d6839c0bbca90d0909b457e48c5938b577f0b391
ludosantana <lucas.d.santana@hotmail.com>
2024-05-08 10:08:06 -0300

    Aula python

--- mundo02/aulas/aula013d.py
+++ mundo02/aulas/aula013d.py
@@ -3,4 +3,6 @@ for c in range(0, 3):
     n = int(input("Digite um valor: ")) #faço a pergunta 3x
     l += [n] #e adiciono os valores na lista
 print(l)
+v = int(input("Qual item da lista gostaria de mostrar? "))
+print(l[v])
 print("FIM")
