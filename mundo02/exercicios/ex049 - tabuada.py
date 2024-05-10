3cbcbd26d05ca672f7cccf7ad4c21f03a868bd5b
ludosantana <lucas.d.santana@hotmail.com>
2024-05-10 16:41:21 -0300

    Aula python

--- mundo02/exercicios/ex049 - tabuada.py
+++ mundo02/exercicios/ex049 - tabuada.py
@@ -1,12 +1,13 @@
-n = 0
+def monta_tabu(n):
+    for c in range(0, 11): #conta de 0 a 11 e monta a tabuada
+        print(f"{n} x {c} = {c * n}")
 
 def ver(i): #verifica a entrada do usuário e atualiza a variavel n
-    global n
     if i.replace(".", "").isdigit():
         if "." in i:
-            n = int(float(i))
+            monta_tabu(int(float(i)))
         else:
-            n = int(i)
+            monta_tabu(int(float(i)))
     else:
         print("Digite um valor numérico corretamente.")
         ver(input("Digite um número: ").strip())
@@ -15,5 +16,4 @@ print("Vamos montar uma TABUADA?")
 
 ver(input("Digite um número: ").strip())
 
-for c in range(0, 11): #conta de 0 a 11 e monta a tabuada
-    print(f"{n} x {c} = {c * n}")
+
