e72caed9556ccd4743150cb369ffbf78eb8ddcef
ludosantana <lucas.d.santana@hotmail.com>
2024-05-10 18:15:26 -0300

    Aula python

--- mundo02/exercicios/ex048.py
+++ mundo02/exercicios/ex048.py
@@ -6,4 +6,6 @@ for c in range(1, l, 2):
     if c % 3 == 0:
         r += c
 
-print(f"A soma entre os {cor['azul']}números ímpares{cor['fecha']}, {cor['amarelo']}múltiplos de três{cor['fecha']}, entre 1 e 500 é:\n\033[7;35m {r:^50} \033[m")
+titulo = "A soma entre os números ímpares, múltiplos de três, entre 1 e 500 é:"
+
+print(f"{titulo}\n\033[1;7;33m {r:^{len(titulo)}} \033[m")
--- mundo02/exercicios/ex049 - tabuada.py
+++ mundo02/exercicios/ex049 - tabuada.py
@@ -1,13 +1,13 @@
 def monta_tabu(n):
     for c in range(0, 11): #conta de 0 a 11 e monta a tabuada
-        print(f"{n} x {c} = {c * n}")
+        print(f"{n} x {c:2} = \033[1;7;32m {c * n:2} \033[m")
 
 def ver(i): #verifica a entrada do usuário e atualiza a variavel n
     if i.replace(".", "").isdigit():
         if "." in i:
             monta_tabu(int(float(i)))
         else:
-            monta_tabu(int(float(i)))
+            monta_tabu(int(i))
     else:
         print("Digite um valor numérico corretamente.")
         ver(input("Digite um número: ").strip())
@@ -15,5 +15,3 @@ def ver(i): #verifica a entrada do usuário e atualiza a variavel n
 print("Vamos montar uma TABUADA?")
 
 ver(input("Digite um número: ").strip())
-
-
