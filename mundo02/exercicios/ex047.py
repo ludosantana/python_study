1748e086ae7a9f2fef68e6299a630ae4908a3453
ludosantana <lucas.d.santana@hotmail.com>
2024-05-14 07:33:05 -0300

    Aula python

--- mundo02/exercicios/ex047.py
+++ mundo02/exercicios/ex047.py
@@ -1,3 +1,8 @@
+for c in range(2, 50+1, 2):
+    print(c, end=" ")
+
+""" Esse modo utiliza mais processamento, pois ele faz um loop extra (dos numeros impares), sem mostrar nada
 for c in range(1, 50+1):
     if c % 2 == 0:
         print(c, end=" ")
+"""
