0e53381d63fcbca13f2f849c5b717bac86864410
ludosantana <lucas.d.santana@hotmail.com>
2024-05-14 07:50:41 -0300

    Aula python

--- mundo02/exercicios/ex048.py
+++ mundo02/exercicios/ex048.py
@@ -3,10 +3,15 @@ r = 0
 l = int(input("Digite um limite para a soma: "))
 i = 0
 
+for c in range(3, l+1, 6):
+    r += c
+    i += 1
+"""
 for c in range(1, l, 2):
     if c % 3 == 0:
         r += c
         i += 1
+"""
 
 titulo = f"A soma entre os {i} números ímpares, múltiplos de três, entre 1 e {l} é:"
 
