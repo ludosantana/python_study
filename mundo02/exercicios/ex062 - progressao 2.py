fb1497b0e23b9811f0d030e3bdfcd6c46d0e4eef
ludosantana <lucas.d.santana@hotmail.com>
2024-08-29 10:30:23 -0300

    Aula python

--- mundo02/exercicios/ex062 - progressao 2.py
+++ mundo02/exercicios/ex062 - progressao 2.py
@@ -18,7 +18,8 @@ def valida_int(mensagem):
             print("Digite um valor numérico.")
 
 while True:
-    pt = valida("Digite o primeiro termo: ")
+    #pt é o primeiro termo da primeira contagem, o pr é o primeiro termo da segunda contagem
+    pr = pt = valida("Digite o primeiro termo: ")
     r = valida("Digite a razão: ")
     c = 0
     limite = 10
@@ -32,11 +33,10 @@ while True:
 
     if limite > 0:
         c = 0 #reseta o contador
-        pt -= 10 #reseta o valor do primeiro termo
         while c < limite:
             c += 1
-            print(f"{pt} →", end=" ")
-            pt += r
+            print(f"{pr} →", end=" ")
+            pr += r
         print("Acabou!")
 
     break
